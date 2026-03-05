40661e0e69fdb633d545f0d959853f98e066f9dc
Matthew Kilgore <m.kilgore0@gmail.com>
2026-03-05 10:27:30 -0500

    Dijkstra now handles multi-tile entities

--- src/navigation/navigator.ts
+++ src/navigation/navigator.ts
@@ -6,8 +6,17 @@ import { NavigationNode } from "./navigation_node";
 import { NodePool } from "./node_pool";
 
 export interface WorldNavigatorOptions {
-     ignoreList: Entity[]
-     pool: NodePool | null
+    width: number
+    height: number
+    ignoreList: Entity[]
+    pool: NodePool | null
+}
+
+const DEFAULT_OPTIONS: WorldNavigatorOptions = {
+    width: 1,
+    height: 1,
+    ignoreList: [],
+    pool: null
 }
 
 export class WorldNavigator {
@@ -17,28 +26,34 @@ export class WorldNavigator {
         this.world = world
     }
 
-    getNavigationGraph(row: number, col: number, options: Partial<WorldNavigatorOptions> = {}) {
-        const DEFAULT_OPTIONS: WorldNavigatorOptions = {
-            ignoreList: [],
-            pool: null
+    isValidMove(row: number, col: number, width: number, height: number, ignoreList: Entity[]) {
+        for (let checkRow = row; checkRow < row + height; checkRow++) {
+            for (let checkCol = col; checkCol < col + width; checkCol++) {
+                if (!this.world.isNavigable(checkRow, checkCol, ignoreList)) {
+                    return false
+                }
+            }
         }
 
+        return true
+    }
+
+    setEdges(node: NavigationNode, width: number, height: number, ignoreList: Entity[]) {
+        for (const dir of DIRS) {
+            node.edges[dir] = this.isValidMove(node.row + TILE_OFFSETS[dir].row, node.col + TILE_OFFSETS[dir].col, width, height, ignoreList)
+        }
+    }
+
+    getNavigationGraph(row: number, col: number, options: Partial<WorldNavigatorOptions> = {}) {
         const fullOptions = { ...DEFAULT_OPTIONS, ...options}
-        const ignoreList = fullOptions.ignoreList
         const pool = fullOptions.pool
 
         const navGraph = new NavigationGrid(this.world.rows, this.world.cols)
 
         const startNode = (pool !== null) ? pool.getNode(row, col) : new NavigationNode(row, col)
-        startNode.distance = 0
+        startNode.distance = 0 
 
-        function setEdges(node: NavigationNode, world: World) {
-            for (const dir of DIRS) {
-                node.edges[dir] = world.isNavigable(node.row + TILE_OFFSETS[dir].row, node.col + TILE_OFFSETS[dir].col, ignoreList)
-            }
-        }
-
-        setEdges(startNode, this.world)
+        this.setEdges(startNode, fullOptions.width, fullOptions.height, fullOptions.ignoreList)
 
         navGraph.setNode(row, col, startNode)
 
@@ -48,7 +63,7 @@ export class WorldNavigator {
             for (const dir of DIRS) {
                 if (finalized.edges[dir] && !navGraph.hasNode(finalized.row + TILE_OFFSETS[dir].row, finalized.col + TILE_OFFSETS[dir].col)) {
                     const newNode = (pool !== null) ? pool.getNode(finalized.row + TILE_OFFSETS[dir].row, finalized.col + TILE_OFFSETS[dir].col) : new NavigationNode(finalized.row + TILE_OFFSETS[dir].row, finalized.col + TILE_OFFSETS[dir].col)
-                    setEdges(newNode, this.world)
+                    this.setEdges(newNode, fullOptions.width, fullOptions.height, fullOptions.ignoreList)
                     navGraph.setNode(finalized.row + TILE_OFFSETS[dir].row, finalized.col + TILE_OFFSETS[dir].col, newNode)
                 }
             }
