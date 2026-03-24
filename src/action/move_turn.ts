aa3f8343704743765b6c1b4ea1479c15f1bff619
Matthew Kilgore <m.kilgore0@gmail.com>
2026-03-24 17:30:52 -0400

    Move under the new paradigm

---
+++ src/action/move_turn.ts
@@ -0,0 +1,36 @@
+import { ECS } from "../ecs"
+import { Entity } from "../entity"
+import { TilePosition, TilePositionSet } from "../position"
+import { ActionDescription, TurnAction } from "./turn_action"
+import { BasicActions } from "./basic_action"
+
+export interface MoveOptions {
+    cooldown: number
+}
+
+const DEFAULT_OPTIONS: MoveOptions = {
+    cooldown: 1200
+}
+
+function moveCallback(entity: Entity, data: {ecs: ECS, destination: TilePosition, source: TilePosition}): ActionDescription {
+    const oldFootprint = entity.footprint()
+    let result = data.ecs.moveEntity(entity, data.destination.row, data.destination.col)
+    return result ? [{
+        turnType: BasicActions.MOVE,
+        turnData: {
+            actorEntity: entity,
+            footprint: new TilePositionSet(oldFootprint, entity.footprint()),
+            destination: data.destination,
+            source: data.source
+        }
+    }] : []
+
+}
+
+export class MoveAction extends TurnAction<{ecs: ECS, destination: TilePosition, source: TilePosition}> {
+    constructor(entity: Entity, ecs: ECS, destination: TilePosition, source: TilePosition, options: Partial<MoveOptions> = {}) {
+        const fullOptions: MoveOptions = { ...DEFAULT_OPTIONS, ...options}
+
+        super(entity, {ecs: ecs, destination: destination, source: source}, fullOptions.cooldown, moveCallback)
+    }
+}
