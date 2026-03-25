461ce91fc769c8fdc9f74ebb1a1a7bf40c3e148f
Matthew Kilgore <m.kilgore0@gmail.com>
2026-03-25 17:33:16 -0400

    Idle turn implemented

---
+++ src/action/idle_turn.ts
@@ -0,0 +1,30 @@
+import { Entity } from "../entity"
+import { ActionDescription, TurnAction } from "./turn_action"
+import { BasicActions } from "./basic_action"
+
+export interface IdleOptions {
+    cooldown: number
+}
+
+const DEFAULT_OPTIONS: IdleOptions = {
+    cooldown: 1200
+}
+
+function idleCallback(entity: Entity, _data: null): ActionDescription {
+    return [{
+        turnType: BasicActions.IDLE,
+        turnData: {
+            actorEntity: entity,
+            footprint: entity.footprint()
+        }
+    }]
+
+}
+
+export class IdleAction extends TurnAction<null> {
+    constructor(entity: Entity, options: Partial<IdleOptions> = {}) {
+        const fullOptions: IdleOptions = { ...DEFAULT_OPTIONS, ...options}
+
+        super(entity, null, fullOptions.cooldown, idleCallback)
+    }
+}
