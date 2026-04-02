b00b6276f2884e92b66e743fe64c7c3c33725106
Matthew Kilgore <m.kilgore0@gmail.com>
2026-04-02 18:34:08 -0400

    Camera listeners for UI support

--- src/camera.ts
+++ src/camera.ts
@@ -1,18 +1,49 @@
 import { Container } from "pixi.js"
 import { GameApp } from "./app"
+import { SignalEmitter } from "./signal"
+
+interface CameraState {
+    x: number,
+    y: number,
+    zoom: number
+}
+
+interface CameraSignals {
+    onMove: SignalEmitter<CameraState>
+    onZoom: SignalEmitter<CameraState>
+}
 
 export class Camera extends Container {
     app: GameApp
     stage: Container
 
+    state: CameraState
+    signals: CameraSignals
+
     constructor(app: GameApp, stage: Container) {
         super()
         this.app = app
         this.stage = stage
+
+        this.state = {
+            x: this.stage.pivot.x,
+            y: this.stage.pivot.y,
+            zoom: this.stage.scale.x
+        }
+
+        this.signals = {
+            onMove: new SignalEmitter<CameraState>(),
+            onZoom: new SignalEmitter<CameraState>()
+        }
     }
 
     setPosition(x: number, y: number) {
-        this.stage.pivot.set(x, y)
+        if (x !== this.state.x || y !== this.state.y) {
+            this.stage.pivot.set(x, y)
+            this.state.x = x
+            this.state.y = y
+            this.signals.onMove.emit(this.state)
+        }
     }
 
     setRotation(rotation: number) {
@@ -24,6 +55,10 @@ export class Camera extends Container {
     }
 
     setZoom(zoom: number) {
-        this.stage.scale.set(zoom)
+        if (zoom !== this.state.zoom) {
+            this.stage.scale.set(zoom)
+            this.state.zoom = zoom
+            this.signals.onZoom.emit(this.state)
+        }
     }
 }
